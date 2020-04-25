0bc42414ea3ad356282dae149cde73b5f98c707e
Noam Waissman <noam.waissman@amdocs.com>
2020-04-25 15:40:28 +0300

    [BRAIN][UPDATE][CLIENT]small bug fix

--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@ionic/react/css/core.css';
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
 
 const render = (Component) =>
   ReactDOM.render(<Component />, document.getElementById("root"));
