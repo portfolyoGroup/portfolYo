dd7770d1156cae9f46f2125827cdd6b3cc0f6a74
noamambar <noamambar@gmail.com>
2020-05-24 21:07:30 +0300

    [CLIENT][FEATURE][NONO][UI] Add prrofile update form. To inspect it, change the flag in App.js to false.

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,12 @@ import { Route, Redirect } from 'react-router-dom'
 import { IonApp, IonRouterOutlet } from '@ionic/react'
 import { IonReactRouter } from '@ionic/react-router'
 import ProfilePage from './pages/ProfilePage/ProfilePage'
+import UpdateProfileInfo from './pages/UpdateProfileInfo/UpdateProfileInfo'
 import './theme/variables.scss';
 
 const App = () => {
     
-    const isLoggedIn = true
+    const goToProfilePage = true
     useEffect(() => {
         const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
         toggleDarkTheme(prefersDark.matches);
@@ -25,11 +26,12 @@ const App = () => {
                     <Route path="/profile:id">
                         <ProfilePage />
                     </Route>
-                    <Route
-                        path="/login"
-                        component={ProfilePage}
-                    />
-                    <Route  path={'/'} render={() => isLoggedIn ? <Redirect to={'/profile1/about'} /> : <Redirect to='/login' />}></Route>
+                    <Route path="/updateProfile">
+                    <UpdateProfileInfo/>
+                    </Route>
+                        
+              
+                    <Route  path={'/'} render={() => goToProfilePage ? <Redirect to={'/profile1/about'} /> : <Redirect to='/updateProfile' />}></Route>
                 </IonRouterOutlet>
             </IonReactRouter>
         </IonApp>
---
+++ client/src/pages/UpdateProfileInfo/UpdateProfileInfo.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonItem, IonLabel, IonList, IonItemDivider } from '@ionic/react';
+import { Route, Switch, useRouteMatch, useParams, Redirect } from 'react-router-dom'
+// import allComponents from '../components'
+
+const dataOfAbout = {
+    description: "Tell us about you:",
+    programing_languages: 'c, Java, etc...',
+    skills: 'react, ',
+    experience: 'mention your current and past roles in projects and jobs'
+}
+const dataOfContact = {
+    date_of_birth: "5.5.1875",
+    mail: 'donotreplay@dnr.com',
+    Adress: 'TLV fashion mall',
+    phone: '051113345'
+}
+const dataOfProfileHome = {
+    name: "Israel Israeli",
+    title: 'your main occpation, e.g cs student',
+    main_description: 'your main occpation, e.g cs student'
+}
+const UpdateProfileInfo = () => {
+    const MyList = ({ dataToRead }) => {
+        return (
+            <IonList>
+                {Object.entries(dataToRead).map(([key, value], index) => {
+                    return (
+                        <IonItem key={index}>
+                            <IonLabel position="floating">{key}</IonLabel>
+                            <IonInput  placeholder={value} clearInput></IonInput>
+                        </IonItem>
+                    )
+                })}
+            </IonList>
+        )
+    }
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Update your profile</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent>
+                <IonItemDivider>Main Profile info</IonItemDivider>
+                <MyList dataToRead={dataOfProfileHome}></MyList>
+                <IonItem class='ion-padding'>
+                <IonLabel position='stacked'> Upload a photo</IonLabel>
+                <IonInput type='file' clearInput></IonInput>
+                </IonItem>
+                <IonItemDivider>About</IonItemDivider>
+                <MyList dataToRead={dataOfAbout}></MyList>
+                <IonItemDivider>Contact</IonItemDivider>
+                <MyList dataToRead={dataOfContact}></MyList>
+            </IonContent>
+        </IonPage>
+    );
+};
+export default UpdateProfileInfo
