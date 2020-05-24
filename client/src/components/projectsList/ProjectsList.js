00e1bece73f69e8b0d894e2ae8daed5b79beb0fd
noamambar <noamambar@gmail.com>
2020-05-24 10:59:25 +0300

    [CLIENT][FEATURE][NONO][UI] Add projects page UI Very temp work.

--- client/src/components/projectsList/ProjectsList.js
+++ client/src/components/projectsList/ProjectsList.js
@@ -1,11 +1,46 @@
 import React from 'react'
-import { IonToolbar, IonTitle, IonContent } from '@ionic/react'
+import { IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonList, IonLabel, IonItemDivider, IonItem, IonText, IonItemSliding } from '@ionic/react'
 import { withRouter } from 'react-router'
+import pic from '../../resources/snakeGamePic.png'
+import '../about/about.scss'
+import '../sharedStyles.scss'
+import '../projectsList/projectsList.scss'
 
 const ProjectsList = () => {
+
+    const OneProj = () => {
+        return (
+            <IonItem>
+                <IonCard>
+                        <img src={pic} />
+                    <IonCardHeader>
+                        <IonCardSubtitle>Cool game</IonCardSubtitle>
+                        <IonCardTitle>Snake</IonCardTitle>
+                    </IonCardHeader>
+
+                    <IonCardContent>
+                        written in cpp
+      </IonCardContent>
+                </IonCard>
+                </IonItem>
+        )
+    }
     return (
         <IonContent>
-            <IonTitle>Project</IonTitle>
+            <IonCard>
+                <IonCardHeader>
+                    <ion-text color="primary">
+                        <h1>My Projects</h1>
+                    </ion-text>
+                </IonCardHeader>
+                <IonCardContent>
+                    <IonList>
+                    <OneProj></OneProj>
+                    <OneProj></OneProj>
+                    <OneProj></OneProj>
+                    </IonList>
+                </IonCardContent>
+            </IonCard>
         </IonContent>
     )
 }
