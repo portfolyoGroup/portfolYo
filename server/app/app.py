b594c996307020bad1819e739de3d51a1383723f
DudiChen <dudi.chen@gmail.com>
2020-08-13 15:39:02 +0300

    aaa

--- server/app/app.py
+++ server/app/app.py
@@ -1,17 +1,16 @@
-from flask import Flask
+from flask import Flask, request
 from app.auth.auth import auth_blueprint
 # from auth import auth_blueprint
 from app.sign_in_up.register import register_blueprint
 # from  import register_blueprint
 from app.project.project import project_blueprint
-# from user.user import user_blueprint
+from app.user import user_blueprint
 
 app = Flask(__name__,)
 app.register_blueprint(register_blueprint)
 app.register_blueprint(auth_blueprint)
 app.register_blueprint(project_blueprint)
-# app.register_blueprint(user_blueprint)
-
+app.register_blueprint(user_blueprint)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   app.run(debug=True)
--- server/app/user/user.py
+++ server/app/user/user.py
@@ -1,22 +1,35 @@
-# from flask import Blueprint, render_template
-# import json
-# from server.service.mongo_db.mongo_client import save_user
-# from server.service.mongo_db.db_entities import User
-#
-# user_blueprint = Blueprint('user_blueprint', __name__, static_folder='../static')
-# headers = {"Content-Type": "application/json"}
-#
-# @user_blueprint.route('/user', methods=['POST'])
-# def create_user():
-#     # Todo: debug, build response, exception handling
+from flask import Blueprint, request, render_template, make_response
+import json
+from server.service.mongo_db.mongo_client import save_user
+from server.service.mongo_db.db_entities import User
+
+user_blueprint = Blueprint('user_blueprint', __name__, static_folder='../static')
+headers = {"Content-Type": "application/json"}
+
+@user_blueprint.route('/user', methods=['POST'])
+def create_user():
+    # # Todo: debug, build response, exception handling
+    # body = json.loads(request.data)
+    # email = body.get('email')
+    # password = body.get('password')
+    # description = body.get('description')
+    # pic = body.get('pic')
+
+    # user = User(email=email, password=password, description=description, pic=pic)
+
+    # save_user(user)
+    id = "123"
+    dictionary = dict()
+    dictionary["id"] = id
+
+    return make_response(json.dumps(dictionary), 200)
+
+    # return "user created successfully "
+
+# @user_blueprint.route('/user', methods=['GET'])
+# def get_user():
 #     body = json.loads(request.data)
 #     email = body.get('email')
 #     password = body.get('password')
-#     description = body.get('description')
-#     pic = body.get('pic')
-#
-#     user = User(email=email, password=password, description=description, pic=pic)
-#
-#     save_user(user)
-#
-#     return "user created successfully "
+
+
