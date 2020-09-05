2db975e31b27e67e898483598d9ea31412cfa127
DudiChen <dudi.chen@gmail.com>
2020-09-05 13:11:08 +0300

    [Server] post merge issues fix 2

--- server/app/user/user.py
+++ server/app/user/user.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, render_template, make_response
 import json
-from service.mongo_db.db_client import mongo_save_user
-from service.mongo_db.db_client import mongo_get_user_by_email
+from service.mongo_db.db_client import save_user
+from service.mongo_db.db_client import get_user_by_email
 from service.mongo_db.db_entities import User
 import uuid
 
@@ -20,7 +20,7 @@ def create_user():
     uid = generate_uid()
     try:
         user = User(email=email, password=password, description=description, pic=pic, uid=uid)
-        mongo_save_user(user)
+        save_user(user)
         dictionary = dict()
         dictionary["id"] = uid
 
@@ -33,7 +33,7 @@ def create_user():
 def get_user():
     body = json.loads(request.data)
     email = body.get('email')
-    user_result = mongo_get_user_by_email(email=email)
+    user_result = get_user_by_email(email=email)
     # TODO: Authentication by email-password is required
 #     password = body.get('password')
     result = dict()
--- server/service/mongo_db/db_client.py
+++ server/service/mongo_db/db_client.py
@@ -11,61 +11,61 @@ from service.mongo_db.db_entities import Project, User
 connect(host=URI)
 
 
-def mongo_save_user(user: User):
+def save_user(user: User):
     try:
         return user.save()
     except NotUniqueError as e:
         raise Exception("User already exist", e)
 
 
-def mongo_get_user_by_id(user_id: str):
+def get_user_by_id(user_id: str):
     try:
         return User.objects(uid=user_id).get()
     except DoesNotExist as e:
         raise DoesNotExist("user with id address " + user_id + " does not exist", e)
 
 
-def mongo_get_user_by_email(email: str):
+def get_user_by_email(email: str):
     try:
         return User.objects(email=email).get()
     except DoesNotExist as e:
         raise DoesNotExist("user with email address " + email + " does not exist", e)
 
 
-def mongo_is_user_exist(user_id: str):  #Todo: change this method implementation
+def is_user_exist(user_id: str):  #Todo: change this method implementation
     try:
-        user = mongo_get_user_by_id(user_id)
+        user = get_user_by_id(user_id)
         return True
     except DoesNotExist as e:
         return False
 
 
-def mongo_update_user(email: str, *args):
+def update_user(email: str, *args):
     pass
 
 
-def mongo_delete_user(email: str):
+def delete_user(email: str):
     try:
         User.objects(email=email).get().delete()
     except DoesNotExist as e:
         raise DbError("user not found", e)
 
 
-def mongo_save_project(project: Project):
+def save_project(project: Project):
     try:
         project.save()
     except Exception as e:
         raise DbError("Could'nt save project", e)
 
 
-def mongo_get_project(pKey: str):
+def get_project(pKey: str):
     try:
         return Project.objects(pKey=pKey).get()
     except Exception as e:
         raise DbError("project not found", e)
 
 
-def mongo_delete_project(pKey: str):
+def delete_project(pKey: str):
     try:
         return Project.objects(pKey=pKey).get().delete()
     except Exception as e:
