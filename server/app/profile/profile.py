75c826e224094867e82b96b6fbc437685c03f1be
Noam Levy <noam.levy@niceactimize.com>
2020-09-10 12:29:26 +0300

    [server][service] - add default profile picture support

--- server/app/profile/profile.py
+++ server/app/profile/profile.py
@@ -1,7 +1,9 @@
 import json
+import os
 
-from flask import request, Blueprint, jsonify, make_response
+from flask import request, Blueprint, jsonify
 from service.mongo_db.db_client import get_user_by_id
+from service.projects_manager.zip_handler import base64_encoder
 
 profile_blueprint = Blueprint('profile_blueprint', __name__)
 
@@ -21,7 +23,6 @@ def convert_user_to_dict(user, uid):
     data_of_contact_dict = dict()
     data_of_profile_home_dict = dict()
     profile_pic_dict = dict()
-    profile_pic_dict["pic"] = user["pic"]
     profile_pic_dict["picName"] = user["picName"]
     profile_pic_dict["picType"] = user["picType"]
     profile_res_dict["picData"] = user["picData"]
@@ -38,7 +39,14 @@ def convert_user_to_dict(user, uid):
     profile_res_dict["dataOfAbout"] = data_of_about_dict
     profile_res_dict["dataOfContact"] = data_of_contact_dict
     profile_res_dict["dataOfProfileHome"] = data_of_profile_home_dict
-    profile_res_dict["profilePic"] = profile_pic_dict
+    profile_res_dict["profilePic"] = profile_pic_dict if profile_pic_dict.get('picName') is not None \
+        else _get_default_pic_dict()
 
     return profile_res_dict
+
+
+def _get_default_pic_dict():
+    encoded_pic = "iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABmJLR0QA/wD/AP+gvaeTAAAGRUlEQVRoge2XbYxUVxnHf+fcOzM7M7sLm+FFdqWgbbEuNalgRQoE0ZYGDKmtQFgNICnSrpWqiVFs07h+oBJSjcZkCTZNGxLZGNqGSGvoNm2hBZGypUqRaFltYNkXwu7Ovs3LfTnn+GF2l9nZ2Z0Jm5I0zu/TzHn+9/88z7n3nnsOlChRokSJEp9gRN7R/QdnoMQSBHdjxCKEmQfMBoJAVZZSAdeAbgwfIk0LPn/mB9/+5yS+qxFiEfB5BNUYM2vYMzys6gdc4CrQBryPMWew1Gke2dw5eQONB1cixE+ANYBV5CTk4yRC/AbfOY6nFeHQV9Dy+wizdgq+GngLxG+p3/TKyOD1BvY1HQYeyL1qXnkF6+beRu30GHPCEcpsG1tIANLKpyuV5IN4N4cvX+Ty0GBRlcyJRPlGzWeprYpREyknKCVRO4DCkPZ9OlNJLvT1cKStlUv5Pc8Qs5aycaPKbqAdqM5W3RWbye5FywnKwpPmG82ulnf4R++1SXVfqJrBrxavIGQV9nS14smzJ/h7Tx5Pnxp21nXI0QEh3s3VbLl14Wjx1RWSJTUWq+bb3F09PrktJFtvW1iwqG233zlafCHPoLTYcmtez1bMtB6A6w08uulBtFWBMQ8CHsD88koAbqmU1M6QVAQFVv7XHoDPDOuHeQjPmkXUK0fIlUBHtqZYz/ljPMXPUe4M6utu5/G1DoA9Rv3YxiHgMPuargHVYTsTnjc90+e/ujVdCY2v8ycrs7PsjD7G43Xx4X9vs68pna0p1jOc7embA+zc2pMdl7kXDJMESPs+AI5v6E0ZrgxOnAjAUSqrgYCXE9ZT9pTWQG58ogY6ATpSCQDe7VCc7VITSK/zQbx75GfX8N38uD0naEBwHOBYZ1vBBCP4RnOgdeT7JV6+KZ5M1IDhOcB7+VIrLd1dBRMNuA6/OHuKiwN9AAMgn74pnkz0VXz1pT7WrU8ZY1Yf62rjaipJmWVTZluEpAVCMOA5XBzo40jbf3jm/Hv8d6gPwEGY9dRvev+meDLRXmiExoM/Qog9QGhSXYZzCB7m0bqWm+k5eQMAv2+qxuZ7wNeBBUCMzPLbC6IdOI3QL9H1YTMNDZOsJx+zZ4kS/6cUfomL4Gffun+hlDxmEF8TcAuQFog3fSmfsrUJadTTArGCzHbiEkYckcJq3H3olfap5p5yA0+sX7PTCPMMmeNmLsnhcTtPrNdgvrPn0GtHp5J/Sg3sWn//diHEs1OwSEmj7939YvNfb9TghhvIPDbiDNcP4zfKFdcOfPHXTUe6C0vHM+7W7lj91TuS6eRfXKXP26Js68ETJ+K5mh9v2BCWDP6JYoo3FJqmTwd87wVg3bB6DPXLl1clLO+PntZ3RsJm7XPNLeez4+Os65Z/qT3peLNSjpnefO5cIjfe0NAg0xdOPS+M2FKodqU0adcjGi68azCGhj0vHv3lRPENSxcnbVv2NL1zZm72+LjdaNLx5kgp0vmK37FjccC98Lf9xRQPEB9KEh9MFiNFCBp2bVjzJPnvlwjYlu+6fvW4QO7AN798l7akEJ+bO/un2N6B7oq+3qr47DsEaqVA1AO1xRQ0lHK4fLUXYwyfik0jVhktqhHgJJj9Rtsny2Tkik9/jRbyiY86erb7SqtDp94b89iPewfCQftKIu3OjQ+l9k4vD++NxWeSWb6Le999penpH6Knf4iAbXsG6OrpD6TSDrOqKgkG8q2oY1gGYpmQCodBQJJMuSTSDpWR8Ee54vENhIPbU65/tP1aXLieT6wyimVNdPLMoLQmlXbpG0oxkExjjCFSFuyMmNAqEXUCiUTwzf5EemZ/Ik00HKKqIkI0FMK2J/fV2tCfSNLVO4BtSV0RtDfnavJO67Z773kgmXQOpFyvEiEIBWxCAZuAJREyk1Qpja8UrufjeP6oWaQs2BEKWXtfeOP077I9v7tq6VNp1/1h0vFio7NnWYRDASwpsK3MoQaj8XyD63ukXA8MhMuCfbFp0YcaXz3+VlENjPDwffdsTjvqEV+pBb5S07TWAW0y10iBtizpWdIasCzRakn5RkUw9IfG5rcnPfRuW7GkVgXkNuWrZUrpBb7SFVprWxsjTcbXWFJ6tm31Bmz5bztg7X/+9VNNk3mWKFGiRIkSn1j+B66D1zq8uwACAAAAAElFTkSuQmCC"
+        # str(base64_encoder(os.path.join(os.getcwd(),'server', 'app','profile', 'statics', 'defaulticon.png')))
+    return {'pic': encoded_pic, 'picName': 'defaulticon', 'picType': 'png'}
 
