24b8ffa5302634379772166a9963918f947c46d8
Noam Levy <noam.levy@niceactimize.com>
2020-09-13 17:17:17 +0300

    [server] - change port to 80

--- server/service/projects_manager/docker_client.py
+++ server/service/projects_manager/docker_client.py
@@ -14,12 +14,13 @@ from service.errors.container_errors import ContainerError
 docker_client = docker.from_env()
 
 def create_image(project_name: str, project_type, user_id: str, project_root):
-    logging.log("going to build image with: ")
     if ' ' in project_name:
         raise NameError("container name must not contain spaces")
     path_to_dockerfile = os.path.join(os.getcwd(), 'server', 'service', 'Dockerimages', project_type)
     buildargs = {"PROJECT_NAME": project_root}
     tag = f"{user_id}_{project_name}"
+    build_command = f"docker build {path_to_dockerfile}/Dockerfile -t {tag.lower()} --build-arg PROJECT_NAME={project_root}"
+    logging.log("going to build image with: " + build_command)
 
     os.system(f"docker build {path_to_dockerfile}/Dockerfile -t {tag.lower()} --build-arg PROJECT_NAME={project_root}")
     logging.log("image built successfully ")
