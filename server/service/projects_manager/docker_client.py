2ffac74f12ab5225661d57b291e9766ea4541d0a
Noam Levy <noam.levy@niceactimize.com>
2020-09-26 16:40:08 +0300

    [server] - edit dockerfile path

--- server/service/projects_manager/docker_client.py
+++ server/service/projects_manager/docker_client.py
@@ -1,4 +1,5 @@
 import logging
+import time
 
 import docker
 import os
@@ -12,7 +13,7 @@ def create_image(project_name: str, project_type, user_id: str, project_root):
     if ' ' in project_name:
         raise NameError("container name must not contain spaces")
     #path_to_dockerfile = os.path.join(os.getcwd(), 'server', 'service', 'Dockerimages', project_type)
-    path_to_dockerfile = os.path.join(os.getcwd(), '..', 'service', 'Dockerimages', project_type)
+    path_to_dockerfile = os.path.join(os.getcwd(), '..', 'service', 'Dockerimages', project_type, os.path.sep)
     buildargs = {"PROJECT_NAME": project_root}
     tag = f"{user_id}_{project_name}"
     build_command = f"docker build -t {tag.lower()} --build-arg PROJECT_NAME={project_root} {path_to_dockerfile}"
@@ -46,3 +47,26 @@ def kill_container(container_tag: str):
     except NotFound as e:
         pass
 
+
+# docker_client.images.build(path="../Dockerimages/node/", buildargs={"PROJECT_NAME": "helloc"}, tag="helloc")
+#
+# container = docker_client.containers.run("helloc", detach=True, tty=True)
+# logs = container.logs()
+# urls = re.findall(b'(?P<url>https?://[^\s]+)', logs)
+# counter = 0
+# while not urls and counter < 20:
+#     urls = re.findall(b'(?P<url>https?://[^\s]+)', logs)
+#     logs = container.logs()
+#     counter += 1
+#     time.sleep(1)
+#
+#
+# url = urls[2].decode("utf-8")
+# url = url.replace("\x1b[K", "")
+# print(url)
+#
+# res = container.exec_run(cmd="echo q")
+# print(res)
+# tmp_log = b'\x1b[?1049h\x1b[22;0;0t\x1b(B\x1b[m\x1b[?1l\x1b>\x1b[H\x1b[2J\x1b[?12l\x1b[?25h\x1b[?1000l\x1b[?1002l\x1b[?1006l\x1b[?1005l\x1b]112\x07\x1b[?25l\x1b[1;1HTip: if you wish to use tmate only for remote access, run: tmate -F        \x1b[30m\x1b[43m[0/0]\x1b[2;1H\x1b[39m\x1b[49mTo see the following messages again, run in a tmate session: tmate show-messages\x1b[3;1HPress <q> or <ctrl-c> to continue\x1b[K\r\n---------------------------------------------------------------------\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[30m\x1b[42m[my_sessio0:sh*                                   "45a992de31a0" 12:47 09-Aug-20\x1b(B\x1b[m\x1b[24;1H\x1b[1;24r\x1b[H\x1b[?12l\x1b[?25h\x1b[?25l\x1b[24d\x1b[30m\x1b[42m[my_sessio0:[tmux]*                               "45a992de31a0" 12:47 09-Aug-20\x1b(B\x1b[m\x1b[1;1H\x1b[?12l\x1b[?25h\x1b[1;23r\x1b[4BConnecting to ssh.tmate.io...\x1b[K\x1b[?25l\x1b[24;1H\x1b[30m\x1b[43m[tmate] Connecting to ssh.tmate.io...                                           \x1b(B\x1b[m\x1b[24;1H\x1b[1;24r\x1b[H\x1b[?12l\x1b[?25h\x1b[1;23r\x1b[5BNote: clear your terminal before sharing readonly access\x1b[K\r\nweb session read only: https://tmate.io/t/ro-Z6ttXZW3E4kw2xDgUXZDZkqxS\x1b[K\r\nssh session read only: ssh ro-Z6ttXZW3E4kw2xDgUXZDZkqxS@lon1.tmate.io\x1b[K\r\nweb session: https://tmate.io/t/jTbWMf25WxcGtHy6thhYsUdFA\x1b[K\r\nssh session: ssh jTbWMf25WxcGtHy6thhYsUdFA@lon1.tmate.io\x1b[K\x1b[?25l\x1b[HTip: if you wish to use tmate only for remote access, run: tmate -F        \x1b[30m\x1b[43m[0/0]\x1b[2;1H\x1b[39m\x1b[49mTo see the following messages again, run in a tmate session: tmate show-messages\x1b[3;1HPress <q> or <ctrl-c> to continue\x1b[K\r\n---------------------------------------------------------------------\x1b[K\r\nConnecting to ssh.tmate.io...\x1b[K\r\nNote: clear your terminal before sharing readonly access\x1b[K\r\nweb session read only: https://tmate.io/t/ro-Z6ttXZW3E4kw2xDgUXZDZkqxS\x1b[K\r\nssh session read only: ssh ro-Z6ttXZW3E4kw2xDgUXZDZkqxS@lon1.tmate.io\x1b[K\r\nweb session: https://tmate.io/t/jTbWMf25WxcGtHy6thhYsUdFA\x1b[K\r\nssh session: ssh jTbWMf25WxcGtHy6thhYsUdFA@lon1.tmate.io\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\x1b[24d\x1b[30m\x1b[43m[tmate] ssh session: ssh jTbWMf25WxcGtHy6thhYsUdFA@lon1.tmate.io                \x1b(B\x1b[m\x1b[24;1H\x1b[1;24r\x1b[H\x1b[?12l\x1b[?25h\x1b[?25lTip: if you wish to use tmate only for remote access, run: tmate -F        \x1b[30m\x1b[43m[0/0]\x1b[2;1H\x1b[39m\x1b[49mTo see the following messages again, run in a tmate session: tmate show-messages\x1b[3;1HPress <q> or <ctrl-c> to continue\x1b[K\r\n---------------------------------------------------------------------\x1b[K\r\nConnecting to ssh.tmate.io...\x1b[K\r\nNote: clear your terminal before sharing readonly access\x1b[K\r\nweb session read only: https://tmate.io/t/ro-Z6ttXZW3E4kw2xDgUXZDZkqxS\x1b[K\r\nssh session read only: ssh ro-Z6ttXZW3E4kw2xDgUXZDZkqxS@lon1.tmate.io\x1b[K\r\nweb session: https://tmate.io/t/jTbWMf25WxcGtHy6thhYsUdFA\x1b[K\r\nssh session: ssh jTbWMf25WxcGtHy6thhYsUdFA@lon1.tmate.io\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n\x1b[K\r\n'
+# web = re.search("web session: .*", str(tmp_log))
+# create_image("pythonWebServer")
