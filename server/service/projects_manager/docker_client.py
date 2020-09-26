a7a957aaa6d75c41e05318b50f0eb2c5a5de8e4d
Noam Levy <noam.levy@niceactimize.com>
2020-09-26 17:35:38 +0300

    [server] - edit dockerfile path

--- server/service/projects_manager/docker_client.py
+++ server/service/projects_manager/docker_client.py
@@ -12,14 +12,14 @@ docker_client = docker.from_env()
 def create_image(project_name: str, project_type, user_id: str, project_root):
     if ' ' in project_name:
         raise NameError("container name must not contain spaces")
-    #path_to_dockerfile = os.path.join(os.getcwd(), 'server', 'service', 'Dockerimages', project_type)
-    path_to_dockerfile = os.path.join(os.getcwd(), '..', 'service', 'Dockerimages', project_type) + os.path.sep
+    path_to_dockerfile = os.path.join(os.getcwd(), 'server', 'service', 'Dockerimages', project_type)
+    # path_to_dockerfile = os.path.join(os.getcwd(), '..', 'service', 'Dockerimages', project_type)
     buildargs = {"PROJECT_NAME": project_root}
     tag = f"{user_id}_{project_name}"
     build_command = f"docker build -t {tag.lower()} --build-arg PROJECT_NAME={project_root} {path_to_dockerfile}"
     logging.error("going to build image with: " + build_command)
 
-    # os.system(build_command)
+    #os.system(build_command)
     image = docker_client.images.build(path=path_to_dockerfile, buildargs=buildargs, tag=tag.lower())
     logging.error("image built successfully ")
 
